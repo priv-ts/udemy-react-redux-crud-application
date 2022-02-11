87f7b7104bd5ce33eab3539213768e15fcde2af7
t.sato <priv.tomokisato@gmail.com>
2022-02-11 16:31:47 +0900

    Learn JSX

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-function App() {
-  return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-);
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return <h1>Hello world</h1>;
+  }
 }
+
+// JSXを使用しない場合
+// class App extends Component {
+//   render() {
+//     return React.createElement(
+//       "div",
+//       null,
+//       "Hello, world!"
+//     );
+//   }
+// }
 
 export default App;
